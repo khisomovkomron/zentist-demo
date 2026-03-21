94c07ecc6df736946f9e7b3c4aba1dd2f93a2cd0
khisomovkomron <normon_9595@bk.ru>
2026-03-21 20:49:19 +0500

    added playwright, test run to url

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -1,2 +1,17 @@
+from playwright.sync_api import sync_playwright
+from dotenv import load_dotenv  
+import os 
 
+load_dotenv() 
 
+class BasePage:
+    
+    def test_setup(self):
+        self.playwright = sync_playwright().start()
+        self.browser = self.playwright.chromium.launch(headless=False)
+        self.page = self.browser.new_page()
+        self.page.goto(os.getenv("BASE_PAGE"))
+
+if __name__ == "__main__":
+    base_page = BasePage()
+    base_page.test_setup()
